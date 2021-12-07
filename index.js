bee1a25752e248ec4706519986395f5af4907811
kevinalbert7 <20ko@live.fr>
2021-12-07 17:29:08 +0100

    formulaire

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-Hello World
+const express = require("express")
+const app = express()
+const port = 5000
+
+const engine = require("express-handlebars").engine
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.render('home')
+})
+
+// app.post('/form/signup', (req, res) => {
+//     console.log(req.body)
+// })
+
+// res.redirect('/http://localhost:5000/home')
+    
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
