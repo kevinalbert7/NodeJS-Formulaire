fed1ccd2ed896cb174f92af2d5966aa385e779fe
kevinalbert7 <20ko@live.fr>
2022-10-27 10:08:22 +0200

    redirection after submit with parameters

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const engine = require("express-handlebars").engine
+const engine = require('express-handlebars').engine
 const port = 5000
 
-app.engine("handlebars", engine())
-app.set("view engine", "handlebars")
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.render('form')
 })
 
-app.post('/form/signup', (req, res) => {
-    console.log(req.body)
-    // res.redirect('/home')
-    res.render('home')
+app.post('/form/profil', (req, res) => {
+    const { username, password } = req.body
+
+    // Pour rediriger vers la route '/profil'
+    // Avec les paramètres url
+    // On démarre la séquence de paramètres avec "?"
+    // parametre_name=parametre_value
+    // On sépare nos paramètres avec "&"
+    res.redirect(`/profil?username=${username}&password=${password}`)
+
+    // Pour afficher directement le template avec les variables
+    // res.render('profil', {
+    //     username: req.body.username,
+    //     password: req.body.password
+    // })   
 })
 
-    
+app.get('/profil', (req, res) => {
+    const { username, password } = req.query
+
+    res.render('profil', {
+        username,
+        password
+    })
+})
+
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
